bc584f9cac83eef182bd1bf46cfc92d0edbb526d
Ma-Ane <142409350+Ma-Ane@users.noreply.github.com>
2024-11-22 18:47:30 +0545

    just check

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -15,7 +15,7 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
 
-  int _selectedIndex = 1 ;
+  int _selectedIndex = 2 ;
 
   void _onItemTapped (int index) {
     setState(() {
